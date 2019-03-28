71c229002e2f78d88310c76b0fbd6dc11726a424
Romain B <romain.bevillard@gandi.net>
2019-03-29 00:55:35 +0100

    SAVEPOINT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import * as express from "express";
-import * as http from "http";
-import * as WebSocket from "ws";
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
 
@@ -21,6 +21,3 @@ server.listen(process.env.PORT || 8999, () => {
   console.log(`started server on port ${server.address().port}`);
 });
 
-server.listen(80, (req, res) => {
-  res.send("GTFO");
-});
